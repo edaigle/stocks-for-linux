293a0c3803736c582bc32f2f054481ce516a9e8c
Eric <eric.daigle@zaclys.net>
2021-06-18 21:14:26 -0400

    finish (re-)implementing API access functions

--- sfl_finnhub_access.c
+++ sfl_finnhub_access.c
@@ -1,51 +1,184 @@
 #include "sfl_finnhub_access.h"
 
-static size_t get_symbol_quote_cb(void *data, size_t size, size_t nmemb, void *userp)
+struct write_result {
+  char *data;
+  int pos;
+};
+
+static size_t curl_write_cb(void *data, size_t size, size_t nmemb, void *userp) {
+  struct write_result *res = (struct write_result*) userp;
+
+  memcpy(res->data + res->pos, data, size * nmemb);
+
+  res->pos += size * nmemb;
+
+  return size * nmemb;
+}
+
+FinnhubFinancialsData* finnhub_get_symbol_financials_data (char *ticker)
 {
-  double *quote = (double*) userp;
+  FinnhubFinancialsData *financials_data = malloc (sizeof (FinnhubFinancialsData));
   
-  cJSON *response_json = cJSON_Parse ( (char*) data);
+  CURL *curl;
+  CURLcode http_res;
+
+  char *api_key = getenv(API_KEY_ENV);
+  char url[80];
+
+  char *buf = malloc (RESPONSE_BUFFER_SIZE);
+  struct write_result api_response = {
+    .data = buf,
+    .pos = 0
+  };
+
+  // Make API call
+  
+  curl = curl_easy_init();
+
+  sprintf (url, API_URL_FINANCIALS, ticker, api_key);
+  
+  curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, curl_write_cb);
+  curl_easy_setopt (curl, CURLOPT_WRITEDATA, &api_response);
+  curl_easy_setopt (curl, CURLOPT_URL, url);
+
+  http_res = curl_easy_perform (curl);
+  if (http_res != CURLE_OK) {
+    fprintf (stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(http_res));
+  }
+
+  buf[api_response.pos] = '\0';
+
+  // Process JSON
+  
+  cJSON *response_json = cJSON_Parse (buf);
   if (!response_json) {
     fprintf (stderr, "CRITICAL: error fetching JSON.\n");
   }
 
-  quote[0] = cJSON_GetObjectItemCaseSensitive(response_json, "c")->valuedouble;
-  quote[1] = cJSON_GetObjectItemCaseSensitive(response_json, "h")->valuedouble;
-  quote[2] = cJSON_GetObjectItemCaseSensitive(response_json, "l")->valuedouble;
-  quote[3] = cJSON_GetObjectItemCaseSensitive(response_json, "o")->valuedouble;
-  quote[4] = cJSON_GetObjectItemCaseSensitive(response_json, "pc")->valuedouble;
-  quote[5] = cJSON_GetObjectItemCaseSensitive(response_json, "t")->valueint;
+  cJSON *metrics = cJSON_GetObjectItemCaseSensitive(response_json, "metric");
 
-  cJSON_Delete (response_json);
+  financials_data->ftw_high = cJSON_GetObjectItemCaseSensitive(metrics, "52WeekHigh")->valuedouble;
+  financials_data->ftw_high_date = strdup (cJSON_GetObjectItemCaseSensitive(metrics, "52WeekHighDate")->valuestring);
+  financials_data->ftw_low = cJSON_GetObjectItemCaseSensitive(metrics, "52WeekLow")->valuedouble;
+  financials_data->ftw_low_date = strdup (cJSON_GetObjectItemCaseSensitive(metrics, "52WeekLowDate")->valuestring);
+  financials_data->dividend_yield = cJSON_GetObjectItemCaseSensitive(metrics, "currentDividendYieldTTM")->valuedouble;
+  financials_data->price_earnings = cJSON_GetObjectItemCaseSensitive(metrics, "peBasicExclExtraTTM")->valuedouble;
 
-  return size * nmemb;
-}
+  // Cleanup and return
   
-double* get_sfl_symbol_quote (char *ticker)
+  cJSON_Delete (response_json);
+  curl_easy_cleanup(curl);
+  free(buf);
+
+  return financials_data;
+}
+
+FinnhubProfileData* finnhub_get_symbol_profile_data (char *ticker)
 {
+  FinnhubProfileData *profile_data = malloc (sizeof (FinnhubProfileData));
+  
   CURL *curl;
-  CURLcode res;
+  CURLcode http_res;
 
-  double *quote = malloc(6 * sizeof(double));
-  char url[78];
+  char *api_key = getenv(API_KEY_ENV);
+  char url[80];
 
-  curl = curl_easy_init ();
-  if (!curl) {
-    fprintf (stderr, "CRITICAL: curl_easy_init() failed.\n");
+  char *buf = malloc (1024); //TODO: Fix magic #
+  struct write_result api_response = {
+    .data = buf,
+    .pos = 0
+  };
+
+  // Make API call
+  
+  curl = curl_easy_init();
+
+  sprintf (url, API_URL_PROFILE, ticker, api_key);
+  
+  curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, curl_write_cb);
+  curl_easy_setopt (curl, CURLOPT_WRITEDATA, &api_response);
+  curl_easy_setopt (curl, CURLOPT_URL, url);
+
+  http_res = curl_easy_perform (curl);
+  if (http_res != CURLE_OK) {
+    fprintf (stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(http_res));
   }
 
-  sprintf (url, API_URL_QUOTE, ticker, getenv(API_KEY_ENV));
+  buf[api_response.pos] = '\0';
 
-  curl_easy_setopt (curl, CURLOPT_URL, url);
-  curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, get_symbol_quote_cb);
-  curl_easy_setopt (curl, CURLOPT_WRITEDATA, quote);
+  // Process JSON
+  
+  cJSON *response_json = cJSON_Parse (buf);
+  if (!response_json) {
+    fprintf (stderr, "CRITICAL: error fetching JSON.\n");
+  }
 
-  res = curl_easy_perform (curl);
-  if (res != CURLE_OK) {
-    fprintf (stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-  }    
+  profile_data->name = strdup (cJSON_GetObjectItemCaseSensitive(response_json, "name")->valuestring);
+  profile_data->industry = strdup (cJSON_GetObjectItemCaseSensitive(response_json, "finnhubIndustry")->valuestring);
+  profile_data->website = strdup (cJSON_GetObjectItemCaseSensitive(response_json, "weburl")->valuestring);
+  profile_data->market_cap = cJSON_GetObjectItemCaseSensitive(response_json, "marketCapitalization")->valuedouble;
 
+  // Cleanup and return
+  
+  cJSON_Delete (response_json);
   curl_easy_cleanup(curl);
+  free(buf);
 
-  return quote;
+  return profile_data;
+}
+
+FinnhubQuoteData* finnhub_get_symbol_quote_data (char *ticker)
+{
+  FinnhubQuoteData *quote_data = malloc (sizeof (FinnhubQuoteData));
+  
+  CURL *curl;
+  CURLcode http_res;
+
+  char *api_key = getenv(API_KEY_ENV);
+  char url[80];
+
+  char *buf = malloc (1024); //TODO: Fix magic #
+  struct write_result api_response = {
+    .data = buf,
+    .pos = 0
+  };
+
+  // Make API call
+  
+  curl = curl_easy_init();
+
+  sprintf (url, API_URL_QUOTE, ticker, api_key);
+  
+  curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, curl_write_cb);
+  curl_easy_setopt (curl, CURLOPT_WRITEDATA, &api_response);
+  curl_easy_setopt (curl, CURLOPT_URL, url);
+
+  http_res = curl_easy_perform (curl);
+  if (http_res != CURLE_OK) {
+    fprintf (stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(http_res));
+  }
+
+  buf[api_response.pos] = '\0';
+
+  // Process JSON
+  
+  cJSON *response_json = cJSON_Parse (buf);
+  if (!response_json) {
+    fprintf (stderr, "CRITICAL: error fetching JSON.\n");
+  }
+
+  quote_data->current_price = cJSON_GetObjectItemCaseSensitive(response_json, "c")->valuedouble;
+  quote_data->daily_high = cJSON_GetObjectItemCaseSensitive(response_json, "h")->valuedouble;
+  quote_data->daily_low = cJSON_GetObjectItemCaseSensitive(response_json, "l")->valuedouble;
+  quote_data->last_close_price = cJSON_GetObjectItemCaseSensitive(response_json, "pc")->valuedouble;
+  quote_data->open_price = cJSON_GetObjectItemCaseSensitive(response_json, "o")->valuedouble;
+  quote_data->timestamp = cJSON_GetObjectItemCaseSensitive(response_json, "t")->valuedouble;
+
+  // Cleanup and return
+  
+  cJSON_Delete (response_json);
+  curl_easy_cleanup(curl);
+  free(buf);
+
+  return quote_data;
 }
--- sfl_finnhub_access.h
+++ sfl_finnhub_access.h
@@ -1,9 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <cjson/cJSON.h>
 #include <curl/curl.h>
 
 #define API_KEY_ENV "LFS_API_KEY"
 
+#define API_URL_CANDLE "https://finnhub.io/api/v1/stock/candle?symbol=%s&resolution=1&from=%s&to=%s&token=%s"
+#define API_URL_FINANCIALS "https://finnhub.io/api/v1/stock/metric?symbol=%s&metric=all&token=%s"
+#define API_URL_PROFILE "https://finnhub.io/api/v1/stock/profile2?symbol=%s&token=%s"
 #define API_URL_QUOTE "https://finnhub.io/api/v1/quote?symbol=%s&token=%s"
+
+#define RESPONSE_BUFFER_SIZE 18300 // Financials file is exactly 18275 bytes, TODO: use realloc
+
+typedef struct {
+  double ftw_high;
+  char* ftw_high_date;
+  double ftw_low;
+  char* ftw_low_date;
+  double dividend_yield;
+  double price_earnings;
+} FinnhubFinancialsData;
+
+typedef struct {
+  char *name;
+  char *industry;
+  char *website;
+  double market_cap;
+} FinnhubProfileData;
+
+typedef struct {
+  double current_price;
+  double daily_high;
+  double daily_low;
+  double last_close_price;
+  double open_price;
+  double timestamp;
+} FinnhubQuoteData;
+  
